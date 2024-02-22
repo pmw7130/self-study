f08ac9f420ede0c77946cf0347d95f019a8d7f6f
pmw7130 <pmw7130@gmail.com>
2024-02-22 14:18:02 +0900

    0221

--- algo/정렬/정렬_수정렬하기3.py
+++ algo/정렬/정렬_수정렬하기3.py
@@ -1,9 +1,10 @@
 import sys
 
 N = int(input())
-arr = []
+arr = [0 for _ in range(N)]
 for i in range(N) :
-    arr.append(int(input()))
+    # arr.append(int(sys.stdin.readline()))
+    arr[i] = int(sys.stdin.readline())
 
 arr.sort()
 for j in arr :
